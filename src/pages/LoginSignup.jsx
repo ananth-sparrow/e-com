8fe443d46b055fa6a1d8db3ef58e8a92cf8f7785
ananth-sparrow <ananthsparrow2001@gmail.com>
2025-02-28 12:46:01 +0530

    frontend complete

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -23,3 +23,30 @@ const LoginSignup = () => {
 }
 
 export default LoginSignup
+
+
+// import React from 'react'
+// import "./Css/loginsignup-login.Css"
+
+// const LoginSignup = () => {
+//   return (
+//     <div className='LoginSignup'>
+//       <div className="LoginSignup-container">
+//         <h1>Sign Up</h1>
+//         <div className="loginsignup-fields">  {/* Updated class name */}
+//           <input type="text" placeholder='Enter your Name' required/>
+//           <input type="email" placeholder='Enter Email address' required/>
+//           <input type="password" placeholder='Password' required/>
+//         </div>
+//         <button>Continue</button>
+//         <p className="loginsignup-login">Already have an account <span>Login here</span></p>
+//         <div className='loginsignup-agree'>
+//           <input type="checkbox" name='' id='' />
+//           <p>By continuing, I agree to the terms of use & privacy policy.</p>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default LoginSignup
